089799331435260035a9b73487542f08d32189de
Rodrigo Robles <rrobles.rodrigo@gmail.com>
2019-11-11 20:01:28 -0500

    search book is functional

---
+++ client/src/pages/searchbooks.js
@@ -0,0 +1,122 @@
+import React, { Component } from "react";
+import Jumbotron from "../components/Jumbotron";
+import Nav from "../components/Nav";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import API from "../utils/API";
+import { BookList, BookListItem } from "../components/BookList";
+import { Container, Row, Col } from "../components/Grid";
+
+class SearchBooks extends Component {
+    //create state
+    state = {
+        bookSearch: "",
+        books: [],
+        error: "",
+        message: ""
+    };
+
+  handleInputChange = event => {
+    // Destructure the name and value properties off of event.target
+    // Update the appropriate state
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  //function to control the submit button of the search form 
+    handleFormSubmit = event => {
+        event.preventDefault();
+        // once it clicks it connects to the google book api with the search value
+        API.getGoogleSearchBooks(this.state.bookSearch)
+            .then(res => {
+                if (res.data.items === "error") {
+                    throw new Error(res.data.items);
+                }
+                else {
+                    // store response in a array
+                    let results = res.data.items
+                    //map through the array 
+                    results = results.map(result => {
+                        //store each book information in a new object 
+                        result = {
+                            key: result.id,
+                            id: result.id,
+                            title: result.volumeInfo.title,
+                            authors: result.volumeInfo.authors,
+                            description: result.volumeInfo.description,
+                            image: result.volumeInfo.imageLinks.thumbnail,
+                            link: result.volumeInfo.infoLink
+                        }
+                        return result;
+                    })
+                    // reset the sate of the empty books array to the new arrays of objects with properties geting back from the response
+                    this.setState({ books: results, error: "" })
+                }
+            })
+            .catch(err => this.setState({ error: err.items }));
+    }
+
+
+  render() {
+    return (
+      <div>
+        <Nav />
+        <Jumbotron />
+        <Container>
+          <Row>
+            <Col size="md-12">
+              <form>
+                <Container>
+                  <Row>
+                    <Col size="xs-9 sm-10">
+                      <Input
+                        name="bookSearch"
+                        value={this.state.bookSearch}
+                        onChange={this.handleInputChange}
+                        placeholder="Search For a Book"
+                      />
+                    </Col>
+                    <Col size="xs-3 sm-2">
+                      <Button
+                        onClick={this.handleFormSubmit}
+                        type="success"
+                        className="input-lg"
+                      >
+                        Search
+                      </Button>
+                    </Col>
+                  </Row>
+                </Container>
+              </form>
+            </Col>
+          </Row>
+          <Row>
+            <Col size="xs-12">
+              {!this.state.books.length ? (
+                <h1 className="text-center">No Books to Display</h1>
+              ) : (
+                <BookList>
+                  {this.state.books.map(book => {
+                    return (
+                      <BookListItem
+                        key={book.id}
+                        title={book.title}
+                        link={book.link}
+                        description={book.description}
+                        image={book.image}
+                      />
+                    );
+                  })}
+                </BookList>
+              )}
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default SearchBooks;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,32 @@
-import axios from "axios";
+import axios from "axios"
 
-// The getRecipes method retrieves recipes from the server
-// It accepts a "query" or term to search the recipe api for
+const googlebooks= "https://www.googleapis.com/books/v1/volumes?q=";
+
 export default {
-  getRecipes: function(query) {
-    return axios.get("/api/recipes", { params: { q: query } });
-  }
-};
+
+    // Get book from google search 
+    getGoogleSearchBooks: function(query) {
+        return axios.get(googlebooks + query)
+    },
+
+    // Gets all books
+    getBooks: function () {
+        return axios.get("/api/books");
+    },
+
+    // Gets the book with the given id
+    getBook: function (id) {
+        return axios.get("/api/books/" + id);
+    },
+
+    // Saves a book to the database
+    saveBook: function (savedBooks) {
+        return axios.post("/api/books", savedBooks);
+    },
+
+    // Deletes the book with the given id
+    deleteBook: function (id) {
+        return axios.delete("/api/books/" + id);
+    }
+
+}
